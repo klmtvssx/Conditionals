dc5a35c72749651ebe1639930e8e8a9a63f3d710
Jibek <kalmatovakyzjibek07@gmail.com>
2025-05-15 16:30:52 +0600

    первый коммит

---
+++ example2.py
@@ -0,0 +1,10 @@
+score = int(input("Введите балл: "))
+
+if score >= 90:
+    print("Отлично!")
+elif score >= 75:
+    print("Хорошо!")
+elif score >= 50:
+    print("Удовлетворительно.")
+else:
+    print("Попробуйте ещё раз.")
